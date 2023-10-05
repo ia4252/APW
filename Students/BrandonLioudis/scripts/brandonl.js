5ba6951d98e964099fb233d4c9df4fee85321347
Blioudis <78106566+Blioudis@users.noreply.github.com>
2023-10-05 18:27:34 -0400

    updated journal and added p5 game

--- Students/BrandonLioudis/scripts/brandonl.js
+++ Students/BrandonLioudis/scripts/brandonl.js
@@ -1,16 +1,15 @@
 // JavaScript goes in this file, javaScript single line comments follow c/c++ standard of two frontslashes
 
-$(document).ready(function() {                                              // jQuery will wait until DOM is ready
+$(document).ready(function() {                                              
 
     // all custom jQuery will go here
 
 
-    $("#MButton1").click(function() {                                       // assign click() function to button using button id
-        $("#jQdemo").css("color","purple");                                        // dynamically sets the style for the specified element
-        $("#jQdemo").html("Brandon Lioudis Project Journal");       // adds the text to the html of the element, $ at front of statement indicates jQuery,                                                                  // # at front of object indicates id, function to be called comes after . 
-        alert("You pressed the Journal button);                                    // call alert just for testing purposes
-        $("#MButton1").html("I created a journal");                             // change the button label, or do whatever you want
-    });                                                                     // note how the jQuery $ identifiers link the html object to the javaScript object
+    $("#MButton1").click(function() {                                       
+        $("#jQdemo").css("color","purple");                                        
+        $("#jQdemo").html("Brandon Lioudis Project Journal: I have embeded a p5 game onto the website in full screen without any borders. The next steps to take are to create more games and create a new website for these games.");     
+                                   
+    });                                                                     
 
 }); // end of jQuery block
 
